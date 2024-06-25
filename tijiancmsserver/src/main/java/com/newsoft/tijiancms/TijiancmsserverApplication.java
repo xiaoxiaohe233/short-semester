d86354f9944ecc1a11b98e12b9ade27bb45ce2b6
isyann123 <isyann@foxmail.com>
2024-06-25 22:32:49 +0800

    Add files via upload

---
+++ tijiancmsserver/src/main/java/com/newsoft/tijiancms/TijiancmsserverApplication.java
@@ -0,0 +1,13 @@
+package com.newsoft.tijiancms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TijiancmsserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TijiancmsserverApplication.class, args);
+    }
+
+}
---
+++ tijiancmsserver/src/test/java/com/newsoft/tijiancms/TijiancmsserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.newsoft.tijiancms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TijiancmsserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
